016aebb468083434f5ba0819c8e2ec4e3653d026
V1C70R264 <victorshirima295@gmail.com>
2025-06-24 21:49:28 +0300

    solving profile page issues

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from .serializers import RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -11,4 +14,12 @@ class RegisterView(generics.CreateAPIView):
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_profile(request):
+    user = request.user
+    return Response({
+        'username': user.username,
+        'email': user.email,
+    })
 # Create your views here.
